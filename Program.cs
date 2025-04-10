f7c89e460131695f4f02319efbc8b263cc149550
erikzon <erikzon@DESKTOP-U5MNQKK>
2025-04-10 15:28:46 -0600

    preparacion para deploy en carpeta

--- Program.cs
+++ Program.cs
@@ -51,10 +51,10 @@ public class Program
         {
             app.UseExceptionHandler("/Error");
             // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-            app.UseHsts();
+            //app.UseHsts();
             app.UseMigrationsEndPoint();
         }
-        app.UseHttpsRedirection();
+        //app.UseHttpsRedirection();
         app.UseStaticFiles();
         app.UseAntiforgery();
         app.MapRazorComponents<App>()
